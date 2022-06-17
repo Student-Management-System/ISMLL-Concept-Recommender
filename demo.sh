db46214989b8b10658baf3c664e2226b22bf6a19
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-06-17 07:46:04 +0200

    ci: Demo start script

---
+++ demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PORT=5000
+
+echo "Starting Concept Map Recommener at $PORT"
+echo "See for documentation: http://localhost:$PORT/"
+
+export FLASK_APP=Webservice.py
+export FLASK_ENV=production
+flask run --host=0.0.0.0 --no-debugger --port=$PORT
