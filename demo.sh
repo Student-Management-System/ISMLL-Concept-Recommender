74368117cc790d8ae67b9cafdd9e09dbfc1e162d
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-06-24 16:07:38 +0200

    Installs requirements for demo system

--- demo.sh
+++ demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PORT=5000
 
-pip install -r requirements
+pip install -r requirements.txt
 echo "Starting Concept Map Recommener at $PORT"
 echo "See for documentation: http://localhost:$PORT/"
 
