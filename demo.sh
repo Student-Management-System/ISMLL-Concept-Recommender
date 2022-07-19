ea3e558a8f2b2fd0809ea5f01287455ef6a49ae5
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-07-19 09:16:04 +0200

    Initial working version of both Group recommenders

--- demo.sh
+++ demo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PORT=5000
 
+# Issue with Pytorch and invalid ELF header, solution based on: https://discuss.pytorch.org/t/importerror-home-name-anaconda3-envs-tf21-lib-python3-7-site-packages-torch-lib-libnvtoolsext-so-1-invalid-elf-header/91764/5
+pip install torch==1.8.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 echo "Starting Concept Map Recommener at $PORT"
 echo "See for documentation: http://localhost:$PORT/"
